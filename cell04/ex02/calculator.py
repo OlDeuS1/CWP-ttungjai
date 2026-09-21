d08d169543367dcb249a8a01ec193e8437483677
OlDeuS <66070083@kmitl.ac.th>
2026-09-21 20:37:47 +0700

    4

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+print("Thank you!")
+print(f"{n1} + {n2} = {n1+n2}")
+print(f"{n1} - {n2} = {n1-n2}")
+print(f"{n1} / {n2} = {n1/n2}")
+print(f"{n1} * {n2} = {n1*n2}")
---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,3 @@
+import math
+n = float(input("Give me a number: "))
+print(math.ceil(n))
