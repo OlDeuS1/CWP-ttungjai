cfdf2c1de87e8f4e304ef0769d9c36617b428b8b
OlDeuS <66070083@kmitl.ac.th>
2026-09-21 21:56:11 +0700

    1.3

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
 firstname = input("Hey, what's your first name? : ")
 lastname = input("And your last name? : ")
-print("Well, pleased to meet you, " + firstname + " " + lastname + ".")
+print("Well, pleased to meet you,", firstname.strip(), lastname.strip(), end=".")
